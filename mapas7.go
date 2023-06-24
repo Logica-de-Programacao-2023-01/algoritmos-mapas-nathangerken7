3f653f5937458e40e69fbed0c5ede83dab3d0852
nathangerken7 <126472627+nathangerken7@users.noreply.github.com>
2023-06-24 14:26:57 -0300

    Create mapas7.go

---
+++ mapas7.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func contarLetrasEPalavras(s string) map[string]map[string]int {
+
+	mF := make(map[string]map[string]int)
+
+	palavras := strings.Fields(s)
+
+	chars := []string{}
+
+	for _, ranP := range palavras {
+
+		chars = strings.Split(ranP, "")
+
+		mF[ranP] = make(map[string]int)
+
+		for _, ranC := range chars {
+
+			mF[ranP][ranC]++
+
+		}
+
+	}
+
+	return mF
+
+}
+
+func main() {
+
+	fmt.Println(contarLetrasEPalavras("socorro alguém me ajuda"))
+
+}
